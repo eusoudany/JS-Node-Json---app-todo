004496bfe3c53f93bcd67d13864bd8beb0d3df89
eusoudany <eusoudany@icloud.com>
2024-09-28 19:55:22 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const start = () =>{
-    let count = 0;
-     while(count < 10){
-        console.log(count);
-        count++
-     }
+const { select } = require('@inquirer/prompts')
+
+const start = async () =>{
+
+    while (true) {
+        
+    
+  const opcao = await select ({ //awit = aguardar
+    message : "Menu >",
+    choices: [ //opcoes de escolhas
+        {
+            name: "Cadastrar meta",
+            value: "cadastrar"
+        },
+        {
+            name: "Listar Metas",
+            value: "listar"
+        },
+        {
+            name: "Sair",
+            value: "sair"
+        }
+    ]
+
+  })
+
+
+   switch (opcao) {
+    case "cadastrar":
+        console.log("Vamos Cadastrar")
+        break;
+    case "listar":
+        console.log("Vamos listar")
+        break;
+    case "sair":
+        console.log("Até a proxima");
+        
+        return
+    default:
+        break;
+   }
+}
 }
 start();
