9d9fd4b32e223c6078b0a14d7df84ecbc1d44d04
eusoudany <eusoudany@icloud.com>
2024-09-28 21:09:18 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-const { select } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+let meta ={
+    value:"Beber Agua",
+    checked: false,
+}
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({message:"Digite a meta"})
+    
+    if (meta.length == 0){
+        console.log("Por favor, digite a meta");
+        return 
+    }
+
+    metas.push(
+        { value: meta, chacked: false }
+    )
+}
+
+const listarMetas = async () => {
+   const respostas = await checkbox ({
+   message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+   choices: [...metas],
+   instructions: false,
+})
+ if(respostas.length == 0){
+    console.log("Nenhuma meta selecionada");
+    return
+ }
+
+
+ metas.forEach((m) => {
+    m.checked = false
+ })
+
+ respostas.forEach((resposta) => {
+    const meta = metas.find((m) => { //find = procurar, ele vai verificar se a resposta é igual a meta
+        return m.value == resposta 
+    })
+    meta.checked = true //marca a resposta como verdadeiro
+ })
+console.log("Metas marcadas como concluidas");
+
+}
 
 const start = async () =>{
 
     while (true) {
         
     
-  const opcao = await select ({ //awit = aguardar
+  const opcao = await select ({ //awit = aguardar (toda vez que tem await, tem que dizer que é async)
     message : "Menu >",
     choices: [ //opcoes de escolhas
         {
@@ -27,10 +72,11 @@ const start = async () =>{
 
    switch (opcao) {
     case "cadastrar":
-        console.log("Vamos Cadastrar")
+        await cadastrarMeta()
+        console.log(metas);
         break;
     case "listar":
-        console.log("Vamos listar")
+        await listarMetas()
         break;
     case "sair":
         console.log("Até a proxima");
